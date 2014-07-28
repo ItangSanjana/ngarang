b42428da34de3b5b85e7e9873ea1d629e9fa901a
iS <arwata@gmail.com>
2014-07-28 21:28:34 +0700

    Modified

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 Drupal.behaviors.ngarang = {
   attach:function(context, settings) {
-    $("input", context).once(function() {
+    $("input", context).once("ngarang", function() {
       if ($(this).attr("size") > 20) {
         $(this).attr("size", 20);
       }
