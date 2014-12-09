9291807c3a998a9d6f18566c8d6d889f89b6d13a
Itang Sanjana <arwata@gmail.com>
2014-12-09 12:32:00 +0700

    Modified if statement

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@
 Drupal.behaviors.ngarang = {
     attach: function (context, settings) {
         $('.grid:first-child').each(function () {
-            if ($(this).length && !$(this).parent().hasClass('grid') && !$(this).parent().hasClass('clearfix')) {
-                $(this).parent().addClass('clearfix');
+            if ($(this).length) {
+                if (!$(this).parent().hasClass('clearfix') || !$(this).parent().hasClass('grid')) {
+                    $(this).parent().addClass('clearfix');
+                }
             }
         });
     }
