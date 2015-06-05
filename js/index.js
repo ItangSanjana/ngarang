05f6fb3ce0a3c420e88410bfc0a05d67a515cc68
Itang Sanjana <arwata@gmail.com>
2015-06-05 18:09:00 +0700

    modified

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 (function ($) {
 
 Drupal.behaviors.ngarang = {
-    attach: function (context, settings) {
-        $('.grid:first-child').each(function () {
-            if ($(this).length) {
-                if (!$(this).parent().hasClass('clearfix') || !$(this).parent().hasClass('grid')) {
-                    $(this).parent().addClass('clearfix');
+    attach: function ( context, settings ) {
+
+        $( '.grid:first-child', context ).each(function() {
+            if ( $( this ).length ) {
+                if ( !$( this ).parent().hasClass( 'clearfix' ) ) {
+                    if ( !$(this).parent().hasClass( 'grid' ) ) {
+                        $( this ).parent().addClass( 'clearfix' );
+                    }
                 }
             }
         });
+
     }
 };
 
